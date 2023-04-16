768e98d48fb30d9a001223f41c6af8b2652547f7
Kiko Perez <kiko243430@gmail.com>
2023-04-15 19:45:27 -0500

    finish what kiko couldn't get done

--- harvest-book/pages/about.js
+++ harvest-book/pages/about.js
@@ -1,14 +1,34 @@
 import React from 'react'
+import styles from '../styles/about.module.css'
 //import { Link } from "react-router-dom";
+import Layout from '../components/Layout';
+import Image from 'next/image';
+
 
 function About() {
     return(
         <>
+        <Layout>
         <div>
-           <h1>About</h1>
-           <p>This is about</p>
-           <a href="/"><button>Return Home</button></a>
+            <div className={styles.clr}></div>
+            <h1 className={styles.Outerbox}>
+                 
+            <img src='/images/farm-img2.jpg' height={200} width={600} />
+            </h1>
+            <div className={styles.GreenBox1}> ABOUT US </div>
+            
+            <div className={styles.Outerbox}>
+            <img src='/images/farm-img1.jpg' height={200} width={600} />
+            </div>
+            <div className={styles.GreenBox2}> MISSION STATEMENT </div>
+            
+
+            <div className={styles.Outerbox1}></div>
+            <div className={styles.GreenBox3}> FOOD WASTE? </div>
+            <div className={styles.GreenBox4}> Why It Matters? </div>
+            <div className={styles.GreenBox5}> Our Solution? </div>
         </div>
+        </Layout>
         </>
     )
 }
--- harvest-book/pages/index.js
+++ harvest-book/pages/index.js
@@ -1,6 +1,5 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import Navbar from '@/components/Navbar'
 import Layout from '@/components/Layout'
 import Head from 'next/head'
 
