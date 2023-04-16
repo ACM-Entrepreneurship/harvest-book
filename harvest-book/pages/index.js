111337657f2a9915d43689ba71937dba6f6c1459
Parsa Hajiha <hajihaparsa2004@gmail.com>
2023-04-15 20:44:31 -0500

    main page final

--- harvest-book/pages/index.js
+++ harvest-book/pages/index.js
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+import Image from 'next/legacy/image'
 import { Inter } from 'next/font/google'
 import Navbar from '@/components/Navbar'
 import Layout from '@/components/Layout'
@@ -17,7 +17,7 @@ export default function Home() {
       </Head>
       <Layout>
       <div className={styles.titlebox}>
-          <Image src='/../public/home_background.jpg' width="1500" height="1000"></Image>
+          <Image src='/../public/images/harvest_book_page.png' layout="fill" height="1000"></Image>
           <div className={styles.titlebg}></div>
           <h1 className={styles.titleText}>Harvest Book</h1>
           <div className={styles.titlebg2}></div>
@@ -39,6 +39,43 @@ export default function Home() {
           </div>
       </div>
       <div className={styles.whitespace}></div>
+
+      <h1 id={styles.charInfoTitle}>Charities</h1>
+
+      <div className="d-flex flex-wrap justify-content-center mx-auto">
+        <div class="card m-3" style={{ width: '22%' }}>
+          <Image src="/../public/images/the-rolla-mission.png" class="card-img-top" alt="..." width={400} height={400} />
+          <div class="card-body">
+            <h5 class="card-title">The Rolla Mission</h5>
+            <p class="card-text">708 N Main St, Rolla, MO 65401 </p>
+            <a href="https://www.therollamission.org/" target="_blank" rel="noopener noreferrer" class="btn btn-success">Learn more</a>
+          </div>
+        </div>
+        <div class="card m-3" style={{ width: '22%' }}>
+          <Image src="/../public/images/feed-my-people.png" class="card-img-top" alt="..." width={400} height={400} />
+          <div class="card-body">
+            <h5 class="card-title">Feed My People</h5>
+            <p class="card-text">171 Kingston Dr, St. Louis, MO 63125 </p>
+            <a href="https://www.feed-my-people.org/" target="_blank" rel="noopener noreferrer" class="btn btn-success">Learn more</a>
+          </div>
+        </div>
+        <div class="card m-3" style={{ width: '22%' }}>
+          <Image src="/../public/images/harvesters.jpg" class="card-img-top" alt="..." width={400} height={400} />
+          <div class="card-body">
+            <h5 class="card-title">Harvesters</h5>
+            <p class="card-text">3801 Topping Ave, Kansas City, MO 64129 </p>
+            <a href="https://www.harvesters.org/" target="_blank" rel="noopener noreferrer" class="btn btn-success">Learn more</a>
+          </div>
+        </div>
+        <div class="card m-3" style={{ width: '22%' }}>
+          <Image src="/../public/images/c-of-c.png" class="card-img-top" alt="..." width={400} height={400} />
+          <div class="card-body">
+            <h5 class="card-title">Circle of Concern</h5>
+            <p class="card-text">112 St Louis Ave, Valley Park, MO 63088</p>
+            <a href="https://www.circleofconcern.org/" target="_blank" rel="noopener noreferrer" class="btn btn-success">Learn more</a>
+          </div>
+        </div>
+      </div>
       </Layout>
     </>
   )
